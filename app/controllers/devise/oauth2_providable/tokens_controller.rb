e403c1321194e78cf7b81052c23a330d1978f7d0
David Oshiro <david@davidoshiro.com>
2012-12-12 15:32:36 -1000

    Clear session before creating new tokens

--- app/controllers/devise/oauth2_providable/tokens_controller.rb
+++ app/controllers/devise/oauth2_providable/tokens_controller.rb
@@ -1,17 +1,29 @@
-class Devise::Oauth2Providable::TokensController < ApplicationController
-  before_filter :authenticate_user!
-  skip_before_filter :verify_authenticity_token, :only => :create
+class TokensController < Devise::Oauth2Providable::TokensController::base
+	before_filter :clear_session
+	before_filter :authenticate_user!
+	skip_before_filter :verify_authenticity_token, :only => :create
+	
+	def create
+		@refresh_token = oauth2_current_refresh_token || oauth2_current_client.refresh_tokens.create!(:user => current_user)
+		@access_token = @refresh_token.access_tokens.create!(:client => oauth2_current_client, :user => current_user)
+		render :json => @access_token.token_response
+	end
 
-  def create
-    @refresh_token = oauth2_current_refresh_token || oauth2_current_client.refresh_tokens.create!(:user => current_user)
-    @access_token = @refresh_token.access_tokens.create!(:client => oauth2_current_client, :user => current_user)
-    render :json => @access_token.token_response
-  end
-  private
-  def oauth2_current_client
-   env[Devise::Oauth2Providable::CLIENT_ENV_REF]
-  end
-  def oauth2_current_refresh_token
-    env[Devise::Oauth2Providable::REFRESH_TOKEN_ENV_REF]
-  end
+	private
+
+	# clear the session, so devise does not use session cookie based auth in any case
+	# the iPhone SDK by default has a shared cookie jar for WebViews and NSURL Request's
+	# and thus will send a cookie to this method.
+	def clear_session
+		session.clear
+	end
+
+	def oauth2_current_client
+		env[Devise::Oauth2Providable::CLIENT_ENV_REF]
+	end
+
+	def oauth2_current_refresh_token
+		env[Devise::Oauth2Providable::REFRESH_TOKEN_ENV_REF]
+	end
+
 end
